b209ce31a3aa485d1566181b48b92496e1014a65
Aristocrat702 <aristo.vika@gmail.com>
2026-07-04 11:54:11 +0500

    1

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,19 +2,8 @@ import Hero from "../components/Hero.jsx";
 
 export default function MainLayout() {
   return (
-    <div style={styles.page}>
+    <div className="app">
       <Hero />
     </div>
   );
 }
-
-const styles = {
-  page: {
-    margin: 0,
-    padding: 0,
-    minHeight: "100vh",
-    background: "#0b0f17",
-    color: "white",
-    fontFamily: "Arial, sans-serif",
-  },
-};
